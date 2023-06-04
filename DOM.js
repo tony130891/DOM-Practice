4659d187d25ef03f5550c659876fcb814c685eaa
Tony <antonivelasquez67@gmail.com>
2023-06-04 10:09:49 -0400

    Incompleted Task manager

---
+++ DOM.js
@@ -0,0 +1,32 @@
+const addTask = document.getElementById("add-task");
+const taskContainer = document.getElementById("task-container");
+const inputTask = document.getElementById("input-task");
+
+// Event listener for the btn so it has a div to insert the tasks
+
+addTask.addEventListener("click", function () {
+  let task = document.createElement("div");
+  task.classList.add("task");
+
+  let li = document.createElement("li");
+  li.innerText = `${inputTask.value}`;
+  task.appendChild(li);
+
+  let checkButton = document.createElement("button");
+  checkButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  checkButton.classList.add("checkTask");
+  task.appendChild(checkButton);
+
+  let deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class="fa-solid fa-check"></i>';
+  deleteButton.classList.add("deleteTask");
+  task.appendChild(deleteButton);
+
+  if (inputTask.value === "") {
+    alert("Please Enter a Task");
+  } else {
+    taskContainer.appendChild(task);
+  }
+
+  inputTask.value = "";
+});
